13e8eccda6fed90c6eea6b2fff7285647ca49fb9
Nan-Buzz <ynxiazj@yeah.net>
2019-07-19 13:53:50 +0800

    原始文件切分之后的微调，就是将大的切成小的，小的合并成大的，每份文件大小在390M左右

--- Xyn_step1.py
+++ Xyn_step1.py
@@ -6,29 +6,26 @@ import pandas as pd
 
 xyn_time_start = time.time()
 ###################################################################
-data = pd.read_json("user_id.json", orient="records", lines=True)
-file_name_list = ['review111.json', 'review222.json', 'review333.json']
-rol = len(data['user_id'])//12
+# file_name = 'review0102'   # 切分
+# jkl = '.json'
+# num = 3  # 拆的份数
+# Xyn_date = pd.read_json(file_name+jkl, orient="records", lines=True)
+# data = Xyn_date[['user_id']].drop_duplicates(keep='first')
+# rol = len(data['user_id'])//num
+# for iii in range(num):
+#     if iii == (num-1): cc = data[rol*iii:]
+#     else: cc = data[rol*iii:rol*(iii+1)]
+#     Xyn_date['new'] = Xyn_date['user_id'].isin(cc['user_id'])
+#     Xyn_date[Xyn_date.new == True][['date', 'review_id', 'text', 'user_id']].to_json(file_name+str(iii)+jkl, orient="records", lines=True)
 
-
-for kuy in range(3):
-    Xyn_date = pd.read_json(file_name_list[kuy], orient="records", lines=True)
-    for iii in range(12):
-        if iii == 11: cc = data[rol*iii:]
-        else: cc = data[rol*iii:rol*(iii+1)]
-        Xyn_date['new'] = Xyn_date['user_id'].isin(cc['user_id'])
-        Xyn_date[Xyn_date.new == True][['date', 'review_id', 'text', 'user_id']].to_json('./'+str(kuy+1)+'/review'+str(iii)+'.json', orient="records", lines=True)
-
-Xyn_date = []
-data = []
-for i in range(12):
-    sys.stdout.write(f'\rcomplete percent:{i:.0f}')
-    dd1 = pd.read_json("./1/review" + str(i) + '.json', orient="records", lines=True)
-    dd2 = pd.read_json("./2/review" + str(i) + '.json', orient="records", lines=True)
-    dd3 = pd.read_json("./3/review" + str(i) + '.json', orient="records", lines=True)
-    tt = pd.concat([dd1, dd2, dd3], axis=0).drop_duplicates(keep='last', subset=["review_id"])
-    tt.to_json('review' + str(i) + '.json', orient="records", lines=True)
-sys.stdout.flush()
+file_name_list = ['review11.json', 'review0101.json', 'review01020.json', 'review01021.json']  # 合并
+out_name = 'review12-011.json'
+we = []
+for i in file_name_list:
+    cc = pd.read_json(i, orient="records", lines=True)
+    we.append(cc)
+end = pd.concat(we)
+end.to_json(out_name, orient="records", lines=True)
 ###################################################################
 xyn_time_end = time.time()
 xyn_minute, xyn_seconds = divmod(xyn_time_end - xyn_time_start, 60)
