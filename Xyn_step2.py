e27a68354b494e0d9c92359ea30ebc5bebb28e93
Nan-Buzz <ynxiazj@yeah.net>
2019-07-20 08:53:22 +0800

    将文件先切分成小份，然后用nlp的包运算，处理好后再合成一个文件

--- Xyn_step2.py
+++ Xyn_step2.py
@@ -9,31 +9,73 @@ import re
 def xyn_io(kk):
     nlp = spacy.load("en_core_web_sm")
     doc = nlp(kk)
+    nlp = []
     text_temp = []
     for ii3 in doc:
         hj = re.findall(r'[A-Za-z]+', ii3.lemma_)
         if hj != []:
             text_temp.append(hj[0].lower())
+            hj = []
     return text_temp
 
 
 xyn_time_start = time.time()
 ###################################################################
-# dir_name_in = "./review-step2"
-# dir_name_out = "./review-step3"
-file_name = 'review12.json'
-# os.makedirs(dir_name_out)
+dir_name_in = "./review-step2"
+dir_name_out = "./review-step3"
+file_name_list = os.listdir(dir_name_in)
+file_name_first = file_name_list[0].split('.')[0]
+file_name_last = '.json'
+os.makedirs(dir_name_out)
 
-cc = pd.read_json(file_name, orient="records", lines=True, chunksize=10000)
-ii_list = []
-for count1, ii1 in enumerate(cc, start=1):
+num = 5
+Xyn_date = pd.read_json(dir_name_in+'/'+file_name_first+file_name_last, orient="records", lines=True, chunksize=1000)
+data = []
+for iii in Xyn_date:
+    data.append(iii)
+Xyn_date = pd.concat(data)
+data = Xyn_date[['user_id']].drop_duplicates(keep='first')
+rol = len(data['user_id'])//num
+for iii in range(num):
+    if iii == (num-1): cc = data[rol*iii:]
+    else: cc = data[rol*iii:rol*(iii+1)]
+    Xyn_date['new'] = Xyn_date['user_id'].isin(cc['user_id'])
+    Xyn_date[Xyn_date.new == True][['date', 'review_id', 'text', 'user_id']].to_json(dir_name_in+'/'+file_name_first+str(iii)+file_name_last, orient="records", lines=True)
+
+file_name_list = os.listdir(dir_name_in)
+file_name_list.remove(file_name_first+file_name_last)
+Xyn_date = []
+data = []
+cc = []
+print("File Segmentation Completion.")
+
+
+for count1, xyn_num in enumerate(file_name_list, start=0):
     sys.stdout.write(f'\rcomplete percent:{count1:.0f}')
-    ii1["text_step1"] = ii1.apply(lambda row: xyn_io(kk=row["text"]), axis=1)
-    ii_list.append(ii1[['user_id', 'text_step1', 'date']])
-
+    cc = pd.read_json(dir_name_in+'/'+xyn_num, orient="records", lines=True, chunksize=100)
+    data = []
+    for ii1 in cc:
+        ii1["text_step1"] = ii1.apply(lambda row: xyn_io(kk=row["text"]), axis=1)
+        data.append(ii1[['user_id', 'text_step1', 'date']])
+    cc = []
+    ii1 = []
+    Xyn_date = pd.concat(data)
+    data = []
+    Xyn_date.to_json(dir_name_out+'/'+file_name_first+str(count1)+file_name_last, orient="records", lines=True)
+    Xyn_date = []
 sys.stdout.flush()
-Xyn_date = pd.concat(ii_list)
-Xyn_date.to_json("review-12.json", orient="records", lines=True)
+print("File processing completed.")
+
+
+file_name_list = os.listdir(dir_name_out)
+data = []
+for ii1 in file_name_list:
+    cc = pd.read_json(dir_name_out+'/'+ii1, orient="records", lines=True)
+    data.append(cc)
+    cc = []
+Xyn_date = pd.concat(data)
+data = []
+Xyn_date.to_json(dir_name_out+'/'+file_name_first+file_name_last, orient="records", lines=True)
 ###################################################################
 xyn_time_end = time.time()
 xyn_minute, xyn_seconds = divmod(xyn_time_end - xyn_time_start, 60)
