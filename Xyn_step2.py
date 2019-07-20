78d53e5fd61438bce0add00f3d9eee97c625b1d2
Nan-Buzz <ynxiazj@yeah.net>
2019-07-20 19:08:44 +0800

    step2.py那份直接读取review-step2文件夹里的文件，处理好后按原来文件名放到输出文件夹，同时会记录哪些文件处理正确，哪些是错误的，文件分割50份，正确保存在right.txt文件中，错误保存在error.txt文件中。text.py文件会生成一个temp文件夹，里面存放正确命名的文件

--- Xyn_step2.py
+++ Xyn_step2.py
@@ -24,31 +24,9 @@ xyn_time_start = time.time()
 dir_name_in = "./review-step2"
 dir_name_out = "./review-step3"
 file_name_list = os.listdir(dir_name_in)
-file_name_first = file_name_list[0].split('.')[0]
-file_name_last = '.json'
 os.makedirs(dir_name_out)
-
-num = 5
-Xyn_date = pd.read_json(dir_name_in+'/'+file_name_first+file_name_last, orient="records", lines=True, chunksize=1000)
-data = []
-for iii in Xyn_date:
-    data.append(iii)
-Xyn_date = pd.concat(data)
-data = Xyn_date[['user_id']].drop_duplicates(keep='first')
-rol = len(data['user_id'])//num
-for iii in range(num):
-    if iii == (num-1): cc = data[rol*iii:]
-    else: cc = data[rol*iii:rol*(iii+1)]
-    Xyn_date['new'] = Xyn_date['user_id'].isin(cc['user_id'])
-    Xyn_date[Xyn_date.new == True][['date', 'review_id', 'text', 'user_id']].to_json(dir_name_in+'/'+file_name_first+str(iii)+file_name_last, orient="records", lines=True)
-
-file_name_list = os.listdir(dir_name_in)
-file_name_list.remove(file_name_first+file_name_last)
-Xyn_date = []
-data = []
-cc = []
-print("File Segmentation Completion.")
-
+txt_error = []
+txt_right = []
 
 for count1, xyn_num in enumerate(file_name_list, start=0):
     sys.stdout.write(f'\rcomplete percent:{count1:.0f}')
@@ -61,21 +39,21 @@ for count1, xyn_num in enumerate(file_name_list, start=0):
     ii1 = []
     Xyn_date = pd.concat(data)
     data = []
-    Xyn_date.to_json(dir_name_out+'/'+file_name_first+str(count1)+file_name_last, orient="records", lines=True)
+    try:
+        Xyn_date.to_json(dir_name_out+'/'+xyn_num, orient="records", lines=True)
+    except:
+        txt_error.append(xyn_num)
+    else:
+        txt_right.append(xyn_num)
     Xyn_date = []
 sys.stdout.flush()
+with open("error.txt", "w", encoding='utf8') as f:
+    for i in txt_error:
+        f.write(i + '\n')
+with open("right.txt", "w", encoding='utf8') as f:
+    for i in txt_right:
+        f.write(i + '\n')
 print("File processing completed.")
-
-
-# file_name_list = os.listdir(dir_name_out)
-# data = []
-# for ii1 in file_name_list:
-#     cc = pd.read_json(dir_name_out+'/'+ii1, orient="records", lines=True)
-#     data.append(cc)
-#     cc = []
-# Xyn_date = pd.concat(data)
-# data = []
-# Xyn_date.to_json(dir_name_out+'/'+file_name_first+file_name_last, orient="records", lines=True)
 ###################################################################
 xyn_time_end = time.time()
 xyn_minute, xyn_seconds = divmod(xyn_time_end - xyn_time_start, 60)
--- Xyn_test.py
+++ Xyn_test.py
@@ -6,30 +6,32 @@ import re
 import pandas as pd
 import sys
 import random
+import psutil
 
 
 xyn_time_start = time.time()
 ###################################################################
-ko = []
-abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-for i in range(26):
-    user_id = '"user_id":"' + abc[i] + '"'
-    for j in range(3):
-        review_id = '"review_id":"'+abc[i]+str(j)+'", '
-        we = ''
-        for h in range(5):
-            we += chr(random.randint(97, 122))
-        text = '"text":"' + we + '", '
-        date = '"date":' + str(random.randint(0, 2000)) + ', '
-        end = '{'+review_id+text+date+user_id+'}'
-        ko.append(end)
-with open("ip2.txt", "a", encoding='utf8') as f:
-    for i in ko:
-        f.write(i + '\n')
+# Xyn_memory = psutil.Process(os.getpid()).memory_full_info().uss / 1048576.
+# print('Memory used: {:.2f} MB'.format(Xyn_memory))
+
+dir_name_in = "./review-step2"
+dir_name_out = "./review-step3"
+dir_name_temp = "./temp"
+file_name_list = os.listdir(dir_name_in)
+file_name_first = sys.argv[1]
+file_name_last = '.json'
+file_name_list.remove(file_name_first+file_name_last)
+os.makedirs(dir_name_temp)
+
+out_num = len(os.listdir(dir_name_out))
+for count1, xyn_num in enumerate(file_name_list, start=0):
+    if count1 == out_num:
+        break
+    os.rename(dir_name_out+'/'+file_name_first+str(count1)+file_name_last, dir_name_temp+'/'+xyn_num)
 ###################################################################
 xyn_time_end = time.time()
 xyn_minute, xyn_seconds = divmod(xyn_time_end - xyn_time_start, 60)
 xyn_hour, xyn_minute = divmod(xyn_minute, 60)
-print("%02d:%02d:%02d" % (xyn_hour, xyn_minute, xyn_seconds))
+print(("%02d:%02d:%02d" % (xyn_hour, xyn_minute, xyn_seconds)))
 
 
